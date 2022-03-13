f6fa906f64882dc5343ee1c25921e0f96c46eda3
jb <36341171+josephbren@users.noreply.github.com>
2022-03-13 17:10:16 -0600

    Added some comments

--- CalculateFibNumber/CalculateFibNumber.cpp
+++ CalculateFibNumber/CalculateFibNumber.cpp
@@ -5,18 +5,22 @@
 #include <unordered_map>
 
 using namespace std;
-typedef long long ll;
+typedef long long ll;	// fib numbers can get quite large
 
 
+/*
+	Memoization should be used to calculate the fibonacci number to cache already computed fib numbers
+	and reuse them instead of recursively recalculating
+*/
 ll CalculateFibonacci(int n, unordered_map<int, ll>& fibMemo)
 {
 	if (n < 0)
-		return -1;
+		return -1;	// Handle edge cases, invalid input
 
 	if (fibMemo.find(n) == fibMemo.end())
 	{
 		if (n <= 1)
-			fibMemo[n] = n;
+			fibMemo[n] = n;		// handle base cases, 0 & 1
 		else
 		{
 			fibMemo[n] = CalculateFibonacci(n - 2, fibMemo) + CalculateFibonacci(n - 1, fibMemo);
